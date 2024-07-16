7256a4b37f1a9bdc768531d4b5f16370f51912e3
MiddleNatural <middlenatural@gmail.com>
2024-07-16 17:26:12 +0700

    Update day4 exercise 2

--- Day4/classExercise2.py
+++ Day4/classExercise2.py
@@ -5,12 +5,12 @@ class CustomerInfo:
     balance = 0
     customerid = ""
     #Creates a method that asks the user to input their email, phone number and balance
-def CustomerInputInfo(self):
-        self.email = input("Email account: ")
-        self.phone = input("Phone number: ")
-        self.balance = input("Balance: ")
-        self.customerid = print(self.email, "|", self.phone, "|", self.balance)
-        print(self.customerid)
+    def CustomerInputInfo(self):
+     self.email = input("Email account: ")
+     self.phone = input("Phone number: ")
+     self.balance = input("Balance: ")
+     self.customerid = print(self.email, "|", self.phone, "|", self.balance)
+     print(self.customerid)
 #Action 1
 def Get_All_Customer():
     fuga = open("Day4/customerdata.txt", "rt")
@@ -24,10 +24,9 @@ def Get_Customer_Based_On_Email(email):
             print(line)
     fuga.close()
 #Action 3
-def Create_CustomerInfo(fuga):
+def Create_CustomerInfo(fuga, self):
     fuga = open("Day4/customerdata.txt", "at")
-    fuga.write(CustomerInputInfo)
-
+    fuga.write(self.email)
 while True:
     nextstep = input("input a number to choose your transaction:\n(1)View all customer info\n(2)Get one customer info based on Email\n(3)Create one customer info (change in data file)\n(4)Update customer balance based on Email (change in data file)\n(5)Delete one customer info based on Email (change in data file)\n(6)Finish\n ")
     if int(nextstep) == 1:
